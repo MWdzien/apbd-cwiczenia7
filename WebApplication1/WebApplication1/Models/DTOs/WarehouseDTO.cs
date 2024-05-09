ed27fe87e47ee1731cb45be332849ca3d2c34d39
Mateusz Wdzienkowski <mateuszwdzienkowski@gmail.com>
2024-05-09 17:13:16 +0200

    Initial project files

---
+++ WebApplication1/WebApplication1/Models/DTOs/WarehouseDTO.cs
@@ -0,0 +1,9 @@
+namespace WebApplication1.Models.DTOs;
+
+public class WarehouseDTO
+{
+    public int IdProduct { get; set; }
+    public int IdWarehause { get; set; }
+    public int Amount { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
